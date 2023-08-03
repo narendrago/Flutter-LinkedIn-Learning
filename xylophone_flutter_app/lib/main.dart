746cbbcf1e1b39298ec31eebc2bd7e7909d65e18
Narendra G O <go.narendra@outlook.com>
2023-08-03 21:17:11 +0530

    Setup Xylophone app with required english_word_package

---
+++ xylophone_flutter_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(child: Text(nouns.first)),
+        ),
+      ),
+    );
+  }
+}
