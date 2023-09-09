dd9d0176bd6a5a4ed210362f63b047668850ee4f
Narendra G O <go.narendra@outlook.com>
2023-09-09 11:49:55 +0530

    Added Flutter Theme

--- bmi_calculator_flutter_app/lib/main.dart
+++ bmi_calculator_flutter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'input_page.dart';
+
 void main() {
   runApp(const BMICalculator());
 }
@@ -11,38 +13,11 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const InputPage(),
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         colorScheme: const ColorScheme.light(
-          primary: Colors.blue,
+          primary: Color(0xFF0A0E21),
         ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.redAccent,
-        ),
-      ),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI Calculator'),
-      ),
-      body: const Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
       ),
     );
   }
